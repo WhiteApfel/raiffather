4ff2ed2e2ad1ee656987d86f6161d3258d2017e6
Eugene Mayer <white@pfel.ru>
2021-10-04 10:44:12 +0300

    Какие глупые опечатки

--- raiffather/modules/sbp_qr.py
+++ raiffather/modules/sbp_qr.py
@@ -4,8 +4,8 @@ from raiffather.modules.base import RaiffatherBase
 class RaiffatherSbpQR(RaiffatherBase):
     async def sbp_qr_pay_prepare(self):
         r = await self._client.get(
-            'https://amobile.raiffeithen.ru/dailybanking/ro/v1.0/c2b/process/transfer',
-            headers=self.authorized_headers
+            "https://amobile.raiffeisen.ru/dailybanking/ro/v1.0/c2b/process/transfer",
+            headers=await self.authorized_headers
         )
         if r.status_code == 200:
             return True
