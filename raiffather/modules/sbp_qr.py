f0cb5ec90a3465b279b6259919d22aa4324cfc37
Eugene Mayer <white@pfel.ru>
2021-10-04 11:26:41 +0300

    Подтверждение пушем

--- raiffather/modules/sbp_qr.py
+++ raiffather/modules/sbp_qr.py
@@ -1,4 +1,4 @@
-from raiffather.models.sbp_qr import SbpQRData
+from raiffather.models.sbp_qr import SbpQRData, SbpQRInit
 from raiffather.modules.base import RaiffatherBase
 
 
@@ -30,6 +30,46 @@ class RaiffatherSbpQR(RaiffatherBase):
             json=data
         )
         if r.status_code == 200:
-            return r.json()
+            return SbpQRInit(**r.json())
         else:
             raise ValueError(f"{r.status_code} {r.text}")
+
+    async def sbp_qr_send_push(self, request_id) -> str:
+        """
+        Отправляет пуш-уведомление для подтверждение и ждёт, когда пуш-сервер его получит
+
+        :param request_id: номер заявки
+        :return: код подтверждения
+        """
+        data = {"deviceUid": self.device.uid, "pushId": self.device.push}
+
+        send_code_response = await self._client.post(
+            f"https://amobile.raiffeisen.ru/dailybanking/ro/v1.0/c2b/process/transfer/{request_id}/push",
+            json=data,
+            headers=await self.authorized_headers,
+        )
+        if send_code_response.status_code == 200:
+            push_id = send_code_response.json()["pushId"]
+            otp = await self.wait_code(push_id)
+            return otp
+        else:
+            raise ValueError(
+                f"{send_code_response.status_code} {send_code_response.text}"
+            )
+
+    async def sbp_qr_push_verify(self, request_id, code) -> bool:
+        """
+        Проверяет код подтверждения
+
+        :param request_id: номер заявки
+        :param code: код подтверждения
+        :return: успешно ли
+        """
+        verify_response = await self._client.put(
+            f"https://amobile.raiffeisen.ru/dailybanking/ro/v1.0/c2b/process/transfer/{request_id}/push",
+            headers=await self.authorized_headers,
+            json={"code": code},
+        )
+        if verify_response.status_code == 204:
+            return True
+        return False
