0d6bc6476a965866999455c7df9fb45760962648
Eugene Mayer <white@pfel.ru>
2021-10-05 23:50:45 +0300

    Исключение для неправильного ответа

--- raiffather/exceptions/base.py
+++ raiffather/exceptions/base.py
@@ -1,3 +1,8 @@
 class RaifException(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, *kwargs)
+
+
+class RaifResponseException(RaifException):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
