7e2b531c0f222cb5f1e4dfae3a8cfddd4e577e8e
Eugene Mayer <white@pfel.ru>
2021-10-15 00:07:37 +0300

    Убрал лишнее переопределение __init__

--- raiffather/exceptions/sbp.py
+++ raiffather/exceptions/sbp.py
@@ -4,9 +4,6 @@ from raiffather.exceptions.base import RaifException
 class SBPRecipientNotFound(RaifException):
     __module__ = "Raiffather"
 
-    def __init__(self, message):
-        super().__init__(message)
-
     def __str__(self):
         return ""
 
