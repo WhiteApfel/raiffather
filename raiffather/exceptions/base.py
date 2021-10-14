78f026c97e504a0bc203585670f2d792fb76ee43
Eugene Mayer <white@pfel.ru>
2021-10-14 22:25:08 +0300

    #6 Перенаправление типа исключения в зависимости от кода ответа и исправления для совместимости

--- raiffather/exceptions/base.py
+++ raiffather/exceptions/base.py
@@ -6,26 +6,43 @@ class RaifException(Exception):
 
 
 class RaifErrorResponse(RaifException):
+    def __new__(cls, response, *args, **kwargs):
+        if response.status_code == 401:
+            return super(RaifErrorResponse, RaifUnauthorized).__new__(RaifUnauthorized, response, *args, **kwargs)
+        if response.status_code == 417:
+            return super(RaifErrorResponse, RaifIncrorrectRequest).__new__(RaifIncrorrectRequest, response, *args, **kwargs)
+        return super(RaifErrorResponse, cls).__new__(cls, response, *args, **kwargs)
+
     def __init__(self, response, *args, **kwargs):
         self.status_code = response.status_code
-        self.text = response.text
+        if "_form" in response.text:
+            self.text = response.json()["_form"]
+            if type(self.text) is list and len(self.text) == 1:
+                self.text = self.text[0]
+        else:
+            self.text = response.text
         self.response = response
         super().__init__(f"{self.status_code}: {self.text}", *args, **kwargs)
 
 
+class RaifIncrorrectRequest(RaifErrorResponse):
+    def __init__(self, response, *args, **kwargs):
+        super().__init__(response, *args, *kwargs)
+
+
 class RaifUnauthorized(RaifErrorResponse):
     def __init__(self, response, *args, **kwargs):
-        super().__init__(*args, *kwargs)
+        super().__init__(response, *args, *kwargs)
 
 
 class RaifDeviceNotVerify(RaifException):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, *kwargs)
+    def __init__(self, response, *args, **kwargs):
+        super().__init__(response, *args, *kwargs)
 
 
 class RaifError3DS(RaifException):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, *kwargs)
+    def __init__(self, response, *args, **kwargs):
+        super().__init__(response, *args, *kwargs)
 
 
 class RaifProductNotFound(RaifException):
@@ -36,5 +53,5 @@ class RaifProductNotFound(RaifException):
 
 
 class RaifPasswordDeprecated(RaifErrorResponse):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, *kwargs)
+    def __init__(self, response, *args, **kwargs):
+        super().__init__(*args, response, *kwargs)
