4910bd628a8523cc6ca630f84689326d76c86669
Eugene Mayer <white@pfel.ru>
2021-10-24 23:31:21 +0300

    ✨ Add support for more types #15

--- raiffather/modules/settings.py
+++ raiffather/modules/settings.py
@@ -1,9 +1,12 @@
+from typing import Union
+
 from raiffather.exceptions.base import RaifErrorResponse
+from raiffather.models.products import Account
 from raiffather.modules.base import RaiffatherBase
 
 
 class RaiffatherSettings(RaiffatherBase):
-    async def set_sbp_settings(self, allow: bool, cba: str):
+    async def set_sbp_settings(self, allow: bool, cba: Union[Account, str, int]):
         """
         Позволяет обновить настройки СБП. Например, привязать другой счёт или включить/отключить приём переводов в Райф
 
@@ -11,7 +14,8 @@ class RaiffatherSettings(RaiffatherBase):
         :param cba: На какой счёт получать. Обязательно указать, даже если отключаете приём переводов
         :return: успешность выполнения
         """
-        data = {"allowTransfers": allow, "cba": cba}
+        account: Account = cba if type(cba) is Account else self.products.accounts[cba]
+        data = {"allowTransfers": allow, "cba": account.cba}
         settings_response = await self._client.post(
             "https://amobile.raiffeisen.ru/rest/1/sbp/settings",
             headers=await self.authorized_headers,
