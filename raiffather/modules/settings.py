95828669249528d116c5079211ae68faec25a4b8
Eugene Mayer <white@pfel.ru>
2021-10-24 23:37:54 +0300

    ✨ Add account currency check (#16)

--- raiffather/modules/settings.py
+++ raiffather/modules/settings.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from raiffather.exceptions.base import RaifErrorResponse
+from raiffather.exceptions.base import RaifErrorResponse, RaifValueError
 from raiffather.models.products import Account
 from raiffather.modules.base import RaiffatherBase
 
@@ -15,12 +15,14 @@ class RaiffatherSettings(RaiffatherBase):
         :return: успешность выполнения
         """
         account: Account = cba if type(cba) is Account else self.products.accounts[cba]
-        data = {"allowTransfers": allow, "cba": account.cba}
-        settings_response = await self._client.post(
-            "https://amobile.raiffeisen.ru/rest/1/sbp/settings",
-            headers=await self.authorized_headers,
-            json=data,
-        )
-        if settings_response.status_code == 200:
-            return True
-        raise RaifErrorResponse(settings_response)
+        if account.currency.id == "RUR":
+            data = {"allowTransfers": allow, "cba": account.cba}
+            settings_response = await self._client.post(
+                "https://amobile.raiffeisen.ru/rest/1/sbp/settings",
+                headers=await self.authorized_headers,
+                json=data,
+            )
+            if settings_response.status_code == 200:
+                return True
+            raise RaifErrorResponse(settings_response)
+        raise RaifValueError("Account currency must be RUR (ruble). It requires a central bank")
